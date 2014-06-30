f8c61c57f0a2f20d0948dc21d36abe4c314c73ac
otknoy <otknoy@gmail.com>
2014-06-30 20:29:41 +0900

    Change retrieval method

--- gotouchi_no_uwasa.rb
+++ gotouchi_no_uwasa.rb
@@ -5,30 +5,30 @@ require 'open-uri'
 require 'nokogiri'
 
 if __FILE__ == $0
-  pagename = '大阪市'
-  uri = URI.escape('http://wiki.chakuriki.net/index.php/' + pagename)
+  pagename = ' 高槻市'
+  # uri = URI.escape('http://wiki.chakuriki.net/index.php/' + pagename)
 
-  doc = Nokogiri::HTML(open(uri))
-  content = doc.css('#bodyContent')
+  # doc = Nokogiri::HTML(open(uri))
+  doc = Nokogiri::HTML(open('takatsuki_city.html'))
 
-  headlines = content.xpath('//*[@class="mw-headline"]').map(&:text)[-3..-1]
+  content = doc.xpath('//div[@class="mw-content-ltr"]')
 
-  uwasa_list = []
-  content.xpath('div/ol').each do |ol|
-    uwasa = ol.xpath('li').map do |li|
-      subject = li.text.split("\n")[0]
-      article = li.text.split("\n")[1..-1].select{|e| e.size != 0}
-      {'subject' => subject, 'articles' => article}
-    end
-    uwasa_list.push(uwasa)
+  elements = content.xpath('*')
+  uwasa_elements = elements.select do |e|
+    e.path =~ /(h[1-6]|ol)/
   end
 
-  
-  headlines.zip(uwasa_list).each do |headline, uwasa|
-    uwasa.each do |u|
-      u['articles'].each do |a|
-        puts "#{pagename},#{headline},#{u['subject']},#{a}"
-      end
+  uwasa_elements.each_slice(2) do |h, ol|
+    puts h.xpath('span[@class="mw-headline"]').text
+    puts
+    ol.xpath('li').each do |li|
+      puts li.text.strip.split(/\n+/)
+      puts
     end
+    puts
   end
+
+  # content.xpath('//*[@class="mw-headline"]').each do |headline|
+  #   puts headline.xpath(
+  # end
 end
