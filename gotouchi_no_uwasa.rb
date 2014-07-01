3ef57b38fe28fe046065a8d04e6745aeadb3128f
otknoy <otknoy@gmail.com>
2014-07-01 17:48:36 +0900

    Implement fetch method and divide it into some methods

--- gotouchi_no_uwasa.rb
+++ gotouchi_no_uwasa.rb
@@ -4,31 +4,55 @@ require 'uri'
 require 'open-uri'
 require 'nokogiri'
 
-if __FILE__ == $0
-  pagename = ' 高槻市'
-  uri = URI.escape('http://wiki.chakuriki.net/index.php/' + pagename)
+BASE_URI = 'http://wiki.chakuriki.net/index.php/'
 
+def fetch_uwasa pagename
+  uri = URI.escape(BASE_URI + pagename)
   doc = Nokogiri::HTML(open(uri))
-  # doc = Nokogiri::HTML(open('takatsuki_city.html'))
 
+  elements = extractUwasaElements doc
+  elements = removeNoOrderListHeading elements
+  elementsToArray(elements)
+end
+
+def extractUwasaElements doc
   content = doc.xpath('//div[@class="mw-content-ltr"]')
-
   elements = content.xpath('*')
-  uwasa_elements = elements.select do |e|
-    e.path =~ /(h[1-6]|ol)/
-  end
+  elements.select {|e| e.path =~ /(h[1-6]|ol)(\[[0-9]+\])*$/}
+end
 
-  uwasa_elements.each_slice(2) do |h, ol|
-    puts h.xpath('span[@class="mw-headline"]').text
-    puts
-    ol.xpath('li').each do |li|
-      puts li.text.strip.split(/\n+/)
-      puts
+def removeNoOrderListHeading elements
+  ret = []
+  elements.each_cons(2) do |e1, e2|
+    pat_h = /h[1-6](\[[0-9]+\])*$/
+    pat_ol = /ol(\[[0-9]+\])*$/
+    ret.push(e1, e2) if e1.path =~ pat_h and e2.path =~ pat_ol
+  end
+  ret
+end
+
+def elementsToArray elements
+  ret = []
+  elements.each_slice(2) do |h, ol|
+    title = h.xpath('span[@class="mw-headline"]').text
+    descriptions = ol.xpath('li').map do |li|
+      li.text.strip.gsub(/\n+/, "\n")
+    end
+    ret.push({'title' => title, 'descriptions' => descriptions})
+  end
+  ret
+end
+
+if __FILE__ == $0
+  pagenames = ['大阪', '大阪市']
+
+  pagenames.each do |p|
+    uwasa_list = fetch_uwasa p
+    puts "pagename: #{p}"
+    uwasa_list.each do |u|
+      puts u['title']
+      puts u['descriptions'].size
     end
     puts
   end
-
-  # content.xpath('//*[@class="mw-headline"]').each do |headline|
-  #   puts headline.xpath(
-  # end
 end
